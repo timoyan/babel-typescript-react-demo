e24f1beeacfcf246454b7e851033649d09761b1e
Timo Yan <tomyan13@gmail.com>
2019-12-02 22:47:15 +0800

    Add emotion.

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,44 @@
 import React from 'react';
-export let z: JSX.Element = <div>Hello world!</div>;
+import styled from 'react-emotion';
+
+const Wrapper = styled('div')({
+    display: 'inline-block',
+    position: 'relative',
+    width: '100%',
+});
+
+const InputWrapper = styled('div')({
+    height: '38px',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '8px 12px',
+    '& svg': {
+        marginLeft: '8px',
+        marginRight: '8px',
+    },
+    '& svg + svg': {
+        marginLeft: '0px',
+    },
+});
+
+const Input = styled('input')({
+    border: 'none',
+    width: '100%',
+    height: '100%',
+    fontSize: '14px',
+    outline: 'none',
+});
+
+export interface ITextBoxProps {}
+
+export const TextBox = React.memo<ITextBoxProps>(({}) => {
+    return (
+        <Wrapper>
+            <InputWrapper>
+                <Input type="input" />
+            </InputWrapper>
+        </Wrapper>
+    );
+});
+
+TextBox.displayName = 'TextBox';
