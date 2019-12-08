aa03efcafb3c06644be25ff0dd7f606e4cc098cf
Timo Yan <tomyan13@gmail.com>
2019-12-08 21:01:50 +0800

    Update lib.

--- packages/share-components/lib/index.js
+++ packages/share-components/lib/index.js
@@ -3,18 +3,16 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "ITextBoxProps", {
-  enumerable: true,
-  get: function get() {
-    return _textbox.ITextBoxProps;
-  }
-});
-Object.defineProperty(exports, "TextBox", {
-  enumerable: true,
-  get: function get() {
-    return _textbox.TextBox;
-  }
-});
 
 var _textbox = require("./textbox");
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUFBIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IHsgSVRleHRCb3hQcm9wcywgVGV4dEJveCB9IGZyb20gJy4vdGV4dGJveCc7XG4iXX0=
+
+Object.keys(_textbox).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _textbox[key];
+    }
+  });
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOztBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQSIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gJy4vdGV4dGJveCc7XG4iXX0=
