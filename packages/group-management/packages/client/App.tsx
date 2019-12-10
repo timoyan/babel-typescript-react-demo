64e472384ea0f40939fcf7619598c0ba68df3fb9
Timo Yan <tomyan13@gmail.com>
2019-12-11 02:59:02 +0800

    Add emotion core and styled packages.

--- packages/group-management/packages/client/App.tsx
+++ packages/group-management/packages/client/App.tsx
@@ -1,11 +1,28 @@
-import { TextBox } from '@demo/share-components/textbox';
+import { TextBox } from '@demo/share-components';
+import { css } from '@emotion/core';
+import { CSSObject } from 'create-emotion';
+import { css as cssfn } from 'emotion';
 import React from 'react';
 
+const color = 'white';
+
+const style: CSSObject = {
+    backgroundColor: 'hotpink',
+};
+
+const newStyle = css({
+    padding: '32px',
+    // backgroundColor: 'red',
+    fontsize: '24px',
+    borderRadius: '4px',
+    ['&:hover']: {
+        color: color,
+    },
+}).styles;
+
 export const App = (): JSX.Element => (
-    <div>
-        <h1>Hello World!</h1>
-        <div>
-            <TextBox name="12" value={'12456'} />
-        </div>
+    <div className={cssfn(style, newStyle)}>
+        Hover to change color <br />
+        <TextBox name="1" className={cssfn({ border: '3px solid green' })} />
     </div>
 );
